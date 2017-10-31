04865ada1dad58bee544e017e0fa4c0703fbbb43
“nasz_login_do_github <nasz_email_do_github>
2017-10-31 11:09:13 +0100

    First Commit

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,10 +1,12 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
-    public static void main (String[] args){
-        Processor processor = new Processor();
-        processor.execute(() -> System.out.println("This is an example test."));
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("abc","nn", System.out.println("b") );
     }
 }
