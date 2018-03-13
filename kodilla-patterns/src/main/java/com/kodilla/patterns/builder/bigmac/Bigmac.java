7bcad1a7d87874edb9c3b8997a6501a0242df02f
“romcioromcio <sobczynskir@wp.pl>
2018-03-13 22:24:29 +0100

    11.5. Builder

--- kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -1,4 +1,126 @@
 package com.kodilla.patterns.builder.bigmac;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bigmac {
+    private static final String ROLL = "Roll without sesame";
+    private static final String ROLL_WITH_SESAME = "Roll with sesame";
+
+    private static final String SAUCE_STANDARD = "Standard sauce";
+    private static final String SAUCE_1000 = "1000 islands sauce";
+    private static final String SAUCE_BARBECUE = "Barbecue sauce";
+
+    private static final String SALAT = "Salat";
+    private static final String ONION = "Onion";
+    private static final String BECON = "Becon";
+    private static final String CUCUMBER = "Cucumber";
+    private static final String CHILI_PEPPER = "Chili pepper";
+    private static final String MUSHROOMS = "Mushrooms";
+    private static final String SHRIMP = "Shrimp";
+    private static final String CHEESE = "Cheese";
+
+    private final String roll;
+    private final int burgers;
+    private final String sauce;
+    private final List<String> ingredients;
+
+    public Bigmac(String roll, int burgers, String sauce, List<String> ingredients) {
+        this.roll = roll;
+        this.burgers = burgers;
+        this.sauce = sauce;
+        this.ingredients = ingredients;
+    }
+
+    public static class BigmacBuilder {
+        private String roll;
+        private int burgers;
+        private String sauce;
+        private List<String> ingredients = new ArrayList<>();
+
+        public BigmacBuilder roll(String roll) {
+            if(roll.equals(ROLL_WITH_SESAME) || roll.equals(ROLL)) {
+                this.roll = roll;
+            }else {
+                throw new IllegalStateException("Incorrect choice! Choose: Roll with sesame or Roll without sesame");
+            }
+            return this;
+        }
+        public BigmacBuilder burgers(int burgers) {
+            this.burgers = burgers;
+            return this;
+        }
+
+        public BigmacBuilder sauce(String sauce) {
+            if(sauce.equals(SAUCE_STANDARD) || sauce.equals(SAUCE_BARBECUE) || sauce.equals(SAUCE_1000)) {
+                this.sauce = sauce;
+            }else {
+                throw new IllegalStateException("Incorrect choice! Choose: Standard sauce or 1000 islands sauce or Barbecue sauce");
+            }
+            return this;
+        }
+
+        public BigmacBuilder ingredient(String ingredient) {
+            if(checkIngredients(ingredient)) {
+                ingredients.add(ingredient);
+            }else {
+                throw new IllegalStateException("Incorrect choice! Available ingredients are: Salat, Onion, Becon, Cucumber, Chili pepper, Mushrooms, Shrimp and Cheese");
+            }
+            return this;
+        }
+
+        public Bigmac build() {
+            return new Bigmac(roll, burgers, sauce, ingredients);
+        }
+
+        private boolean checkIngredients(String ingredient) {
+            boolean check = false;
+            List<String> availableIngredients = availableIngredients();
+            for(String ing : availableIngredients) {
+                if(ing.contains(ingredient)) {
+                    check = true;
+                }
+            }
+            return check;
+        }
+
+        private List<String> availableIngredients() {
+            List<String> ingredientsList = new ArrayList<>();
+            ingredientsList.add(SALAT);
+            ingredientsList.add(ONION);
+            ingredientsList.add(BECON);
+            ingredientsList.add(CUCUMBER);
+            ingredientsList.add(CHILI_PEPPER);
+            ingredientsList.add(MUSHROOMS);
+            ingredientsList.add(SHRIMP);
+            ingredientsList.add(CHEESE);
+            return ingredientsList;
+        }
+    }
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public int getBurgers() {
+        return burgers;
+    }
+
+    public String getSauce() {
+        return sauce;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return "Bigmac{" +
+                "roll='" + roll + '\'' +
+                ", burgers=" + burgers +
+                ", sauce='" + sauce + '\'' +
+                ", ingredients=" + ingredients +
+                '}';
+    }
 }
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,48 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+
+    @Test
+    public void testBigMac() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .roll("Roll without sesame")
+                .burgers(2)
+                .sauce("1000 islands sauce")
+                .ingredient("Salat")
+                .ingredient("Cheese")
+                .ingredient("Becon")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyBurgers = bigmac.getBurgers();
+        int howManyIngredients = bigmac.getIngredients().size();
+        String whichRoll = bigmac.getRoll();
+        String whichSauce = bigmac.getSauce();
+        //Then
+        Assert.assertEquals(2, howManyBurgers);
+        Assert.assertEquals(3, howManyIngredients);
+        Assert.assertEquals("Roll without sesame", whichRoll);
+        Assert.assertEquals("1000 islands sauce", whichSauce);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testBigMacIncorrectInputs() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .roll("Roll without sesame")
+                .sauce("Mayonnaise")
+                .ingredient("Chili pepper")
+                .build();
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        String whichSauce = bigmac.getSauce();
+        //Then
+        Assert.assertEquals(0, howManyIngredients);
+        Assert.assertNotEquals("Roll with sesame", bigmac.getRoll());
+        Assert.assertNotEquals("Barbecue sauce", whichSauce);
+    }
+}
